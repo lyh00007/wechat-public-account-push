b9e97affcbc0d410e669de8c487921e57d376ff5
lyh00007 <127460878+lyh00007@users.noreply.github.com>
2023-09-08 23:29:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -16,31 +16,7 @@ const USER_CONFIG = {
   PROVINCE: '广东',
   CITY: '韶关',
 
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '肾虚仔',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oDRxy6c1R2KSWycod0vV9E4EU0a0',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'zEz8HpeN9c8pviNdI9lo6fHHYknWupB4vzOmM_5HJgU',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '07-30',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '肾虚', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-
-  
-      ],
+  USERS: 
 
       [
     {
