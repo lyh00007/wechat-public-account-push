8f8ea9066a085dee13876ba464dfa229d30db4ae
lyh00007 <127460878+lyh00007@users.noreply.github.com>
2023-09-09 00:58:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -47,6 +47,31 @@ const USER_CONFIG = {
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
+
+     {
+      // 想要发送的人的名字
+      name: 'da号',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oDRxy6RQ_KsE1pppOcr-9mxD2_M0',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'zEz8HpeN9c8pviNdI9lo6fHHYknWupB4vzOmM_5HJgU',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-27',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: 'da号', year: '1996', date: '09-31',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+        },
+      ],
+
+    },
   ],
 
    
